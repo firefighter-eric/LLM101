6ccb83b857045bf4a1504743e7c8b782df8edc4c
Qifan Zhang <firefighter-eric@users.noreply.github.com>
2025-05-03 00:27:55 +0800

    feat: ms-swift sft

---
+++ training/download_models.sh
@@ -0,0 +1,2 @@
+huggingface-cli download Qwen/Qwen3-0.6B --local-dir data/models/Qwen/Qwen3-0.6B
+huggingface-cli download Qwen/Qwen2.5-0.5B-Instruct --local-dir data/models/Qwen/Qwen2.5-0.5B-Instruct
---
+++ training/sft.sh
@@ -0,0 +1,28 @@
+clear
+
+export WANDB_PROJECT=llm101
+
+CUDA_VISIBLE_DEVICES=0 \
+swift sft \
+    --model data/models/Qwen/Qwen2.5-0.5B-Instruct \
+    --train_type full \
+    --dataset data/swift/Qwen3-SFT-Mixin/qwen3_32b_distill_1k.jsonl \
+    --torch_dtype bfloat16 \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --learning_rate 1e-5 \
+    --gradient_accumulation_steps 16 \
+    --eval_steps 50 \
+    --save_steps 50 \
+    --save_total_limit 1 \
+    --logging_steps 1 \
+    --max_length 2048 \
+    --output_dir runs/test/qwen2.5-0.5b \
+    --system 'You are a helpful assistant.' \
+    --warmup_ratio 0.05 \
+    --dataloader_num_workers 4 \
+    --report_to wandb \
+    --attn_impl flash_attn \
+    --use_liger_kernel true
+    # --tuner_backend unsloth
